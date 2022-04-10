c5351c9587d3902a6229ff198cfe23c885055f97
Soumitra Bera <soumya9396@gmail.com>
2022-04-10 11:42:27 +0530

    First commit

--- UIPickerViewExample/ViewController.swift
+++ UIPickerViewExample/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
+enum Direction: String, CaseIterable {
+   case North
+   case South
+   case East
+   case West
+}
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var pickerData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pickerData = ["North","South","East","West"]
     }
-
-
 }
-
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        debugPrint(pickerData[row])
+    }
+}
